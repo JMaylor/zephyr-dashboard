e750b89e50e7b563e002730cc76e672fcefc3077
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-09-20 17:40:29 +0100

    chart

---
+++ src/components/DashboardCard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="rounded-md shadow-md w-full p-4">
+    <slot />
+  </div>
+</template>
---
+++ src/components/VChart.vue
@@ -0,0 +1,18 @@
+<template>
+  <DoughnutChart :chartData="testData" />
+</template>
+
+<script setup>
+import { DoughnutChart } from 'vue-chart-3';
+import { Chart, DoughnutController, ArcElement, Tooltip, Legend } from 'chart.js';
+Chart.register(DoughnutController, ArcElement, Tooltip, Legend);
+
+const testData = {
+  datasets: [
+    {
+      data: [30, 40, 60, 70, 5],
+      backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
+    },
+  ],
+};
+</script>
