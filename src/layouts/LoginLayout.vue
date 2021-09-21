bd12703f5866bf7df711d6676b36768fc24b4c91
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-09-21 20:04:50 +0100

    remove unneeded file

--- src/layouts/LoginLayout.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="bg-blue-200 h-screen w-screen flex items-center justify-center">
-    <div class="bg-white px-8 py-4 shadow-lg rounded-lg">
-      <slot></slot>
-    </div>
-  </div>
-</template>
