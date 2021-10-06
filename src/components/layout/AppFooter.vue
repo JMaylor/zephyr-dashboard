4f12a4ab98a928c666194bbffd0cae5d79e65c04
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-10-06 22:49:11 +0100

    footer styling

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,3 +1,7 @@
 <template>
-  <footer class="text-center p-4">footer content</footer>
+  <footer class="text-center px-4">
+    <div class="border-t border-zinc-300 dark:border-zinc-700 py-2">
+      Made with ❤️ by Joe Maylor - 2021
+    </div>
+  </footer>
 </template>
