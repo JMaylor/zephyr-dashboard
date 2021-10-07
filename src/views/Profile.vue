ee1a49bfeb5e5d993587f4bafea00f77558ed8ef
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-10-07 14:02:37 +0100

    Update Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,13 +1,17 @@
-<template>Profile</template>
+<template>
+  <div v-if="user">
+    <img
+      :src="`https://avatars.dicebear.com/api/avataaars/${user.name.first}.svg?radius=50`"
+      alt="avatar"
+      class="w-72 h-72 drop-shadow"
+    />
+  </div>
+</template>
 
 <script setup>
 const user = ref(null);
 
-async function fetchRandomUser() {
-  user.value = await fetch("https://randomuser.me/api/")
-    .then((response) => response.json())
-    .then(({ results: [user] }) => user);
-}
-
-onMounted(fetchRandomUser);
+fetch("https://randomuser.me/api/")
+  .then((response) => response.json())
+  .then(({ results }) => (user.value = results[0]));
 </script>
