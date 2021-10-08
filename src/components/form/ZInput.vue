c34987aa559f1abff5219b9e9febfe37e7d9e9f3
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-10-08 23:02:18 +0100

    add text input component

---
+++ src/components/form/ZInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="z-input mt-1">
+    <label class="pl-2 py-2 block h-8 sm:text-sm">{{ label }}</label>
+    <input
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="
+        w-full
+        py-2
+        px-3
+        text-left
+        bg-zinc-50
+        dark:bg-black
+        rounded-lg
+        shadow-lg
+        focus:outline-none
+        focus-visible:ring-2
+        focus-visible:ring-opacity-75
+        focus-visible:ring-teal-800
+        dark:focus-visible:ring-teal-500
+        sm:text-sm
+      "
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: false,
+  },
+  modelValue: {
+    required: true,
+  },
+});
+</script>
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,32 +1,28 @@
 <template>
-  <form class="grid grid-cols-12 gap-3">
+  <form class="grid grid-cols-12 gap-x-3 gap-y-0">
     <ZSelect
-      class="
-        col-span-12
-        sm:col-span-6
-        md:col-span-4
-        lg:col-span-3
-        xl:col-span-2
-      "
-      label="Person"
-      :items="people"
-      v-model="selectedPerson"
-      itemText="name"
+      class="form-col"
+      label="Title"
+      :items="titles"
+      v-model="user.title"
     />
+    <ZInput class="form-col" label="Forename" v-model="user.forename" />
+    <ZInput class="form-col" label="Surname" v-model="user.surname" />
   </form>
 </template>
 
 <script setup>
-import { ref } from "vue";
+const titles = ["Mr", "Mrs", "Miss", "Ms", "Dr"];
 
-const people = [
-  { name: "Wade Cooper" },
-  { name: "Arlene Mccoy" },
-  { name: "Devon Webb" },
-  { name: "Tom Cook" },
-  { name: "Tanya Fox" },
-  { name: "Hellen Schmidt" },
-];
-
-const selectedPerson = ref();
+const user = ref({
+  title: null,
+  forename: "",
+  surname: "",
+});
 </script>
+
+<style scoped>
+.form-col {
+  @apply col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-2;
+}
+</style>
