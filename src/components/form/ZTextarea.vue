5070b175130e4c5223dc60aa0c1e6578a563023f
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-10-09 23:06:40 +0100

    lint

--- src/components/form/ZTextarea.vue
+++ src/components/form/ZTextarea.vue
@@ -2,7 +2,7 @@
   <div>
     <label :for="id" class="pl-2 py-2 block h-8 sm:text-sm">{{ label }}</label>
     <textarea
-    placeholder="Tell me something about you!"
+      placeholder="Tell me something about you!"
       ref="textarea"
       rows="3"
       :id="id"
@@ -18,10 +18,8 @@
         dark:bg-black
         rounded-lg
         shadow-lg
-        focus:outline-none
-        focus:ring-2
-        ring-opacity-75
-        ring-teal-800
+        focus:outline-none focus:ring-2
+        ring-opacity-75 ring-teal-800
         dark:ring-teal-500
         sm:text-sm
       "
