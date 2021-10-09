fa4ddb209f857b7633474396439a34a188af23b5
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-10-09 12:00:42 +0100

    restyle form and add gender dropdown

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="grid grid-cols-12 gap-x-3 gap-y-0">
+  <form class="grid grid-cols-12 gap-3 mx-auto max-w-screen-xl">
     <ZSelect
       class="form-col"
       label="Title"
@@ -8,21 +8,29 @@
     />
     <ZInput class="form-col" label="Forename" v-model="user.forename" />
     <ZInput class="form-col" label="Surname" v-model="user.surname" />
+    <ZSelect
+      class="form-col"
+      label="Gender"
+      :items="genders"
+      v-model="user.gender"
+    />
   </form>
 </template>
 
 <script setup>
 const titles = ["Mr", "Mrs", "Miss", "Ms", "Dr"];
+const genders = ["Male", "Female", "Other"];
 
 const user = ref({
   title: null,
-  forename: "",
-  surname: "",
+  forename: null,
+  surname: null,
+  gender: null,
 });
 </script>
 
 <style scoped>
 .form-col {
-  @apply col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-2;
+  @apply col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3;
 }
 </style>
